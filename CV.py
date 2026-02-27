43e9dd3c76e3e8294740fea02df3a88f70f296fd
awadiouf260602 <awadiouf260602@gmail.com>
2026-02-27 12:37:17 +0000

    Update CV.py

--- CV.py
+++ CV.py
@@ -12,20 +12,6 @@ with st.sidebar:
     st.write("2022-2025: Licence en Geographie")
     st.write("2025-2026:BTS en Geomatique")
 
-st.header("Technicien supérieur en Geomatique")
-
-st.subheader("A propos de moi")
-st.write("Passionné par la Geographie, la Géomatique et la Cartographie, je suis une personne rigoureuse dotée d'une capacité de travailler en equipe pour mener à bien des projets")
-
-st.subheader("Projets académiques")
-st.write("Programmation: Réalisation d'un CV digital sur Steamlit")
-st.write("Projection cartographique: Collecte de données aux environs de la Mosquée Masalik djinane")
-st.write("Base de données: Georeferencement d'une carte, création d'une base de données spatiales")
-st.write("Technique de base de la Géographie: réalisation de cartes ( themeatiques et de situation)")
-
-st.subheader("Expériences")
-st.write("Poste d'adjoint president de la communication de l'amical des etudiants thièsois")
-st.write("Poste de commissaire au compte dans un GIE nommé OJEUF")
 
 
 st.subheader("Compétences")
@@ -41,3 +27,4 @@ st.subheader("Perspectives")
 st.write("Ambitionne de créer un cabinet d'expertise en Géomatique spécialisé dans L'Amenagement du territoire")
 st.write("Ambitionne de concevoir et d'administrer des bases de données spatiales (post gis) pour l'analyse de données geographiques")
 st.write("Specialiste SiG")
+
